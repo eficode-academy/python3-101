3b7ea00178acbcfccbe60bfa8ba5422b3f41ab08
tanskann <niklas.tanskanen@eficode.com>
2020-10-01 11:12:38 +0300

    remove DeprecationWarning

--- phonebook/test_phonebook.py
+++ phonebook/test_phonebook.py
@@ -5,27 +5,27 @@ import phonebook
 
 
 class TestPhonebook(unittest.TestCase):
-    
+
     def setUp(self):
         phonebook.clear()
-    
+
     def test_add_name(self):
         phonebook.add("Eric Example", "+55 38 5588")
-        
+
     def test_add_and_get(self):
         self._verify_add_and_get("Eric Example", "+55 123 456")
         self._verify_add_and_get("Maria", "358 44")
         self._verify_add_and_get("a", "1")
 
     def _verify_add_and_get(self, name, number):
-        self.assertEquals(phonebook.add(name, number), True)
-        self.assertEquals(phonebook.get(name), number)
+        self.assertEqual(phonebook.add(name, number), True)
+        self.assertEqual(phonebook.get(name), number)
 
     def test_get_non_existing_name(self):
         self._verify_number("Not there", "Unknown")
-        
+
     def _verify_number(self, name, number):
-        self.assertEquals(phonebook.get(name), number)
+        self.assertEqual(phonebook.get(name), number)
 
     def test_adding_illegal_numbers(self):
         self._verify_not_added("Illegal", "not valid")
@@ -39,25 +39,25 @@ class TestPhonebook(unittest.TestCase):
         self._verify_not_added("", "123")
 
     def _verify_not_added(self, name, number):
-        self.assertEquals(phonebook.add(name, number), False)
-        self.assertEquals(phonebook.get(name), "Unknown")
+        self.assertEqual(phonebook.add(name, number), False)
+        self.assertEqual(phonebook.get(name), "Unknown")
 
     def test_clear(self):
         phonebook.add("name", "123")
         phonebook.clear()
         self._verify_number("name", "Unknown")
-        
+
     def test_empty_book_to_string(self):
-        self.assertEquals(phonebook._to_string(), "")
-        
+        self.assertEqual(phonebook._to_string(), "")
+
     def test_serialize_to_string(self):
         self._add_test_names()
-        self.assertEquals(phonebook._to_string(), self.TEST_NAMES_SERIALIZED)
-        
+        self.assertEqual(phonebook._to_string(), self.TEST_NAMES_SERIALIZED)
+
     def test_read_from_string(self):
         phonebook._read(self.TEST_NAMES_SERIALIZED)
         self._verify_test_names()
-    
+
     def test_save_and_read_from_file(self):
         self._add_test_names()
         phonebook.save("test.temp")
@@ -74,7 +74,7 @@ Name=123
         phonebook.add("Name", "123")
         phonebook.add("John Doe", "+555 123")
 
-    def _verify_test_names(self):  
+    def _verify_test_names(self):
         self._verify_number("Name", "123")
         self._verify_number("John Doe", "+555 123")
 
--- strings/test_strings.py
+++ strings/test_strings.py
@@ -2,27 +2,27 @@ import unittest
 import strings
 
 class TestStrings(unittest.TestCase):
-    
+
     def test_get_string_length(self):
-        self.assertEquals(strings.length(''), 0)
-        self.assertEquals(strings.length('foo'), 3)
-        
+        self.assertEqual(strings.length(''), 0)
+        self.assertEqual(strings.length('foo'), 3)
+
     def test_get_string_without(self):
         # implement strings.without that will remove given characters
-        self.assertEquals(strings.without('abba', 'a'), 'bb')
-        self.assertEquals(strings.without('abba', 'cd'), 'abba')
-        self.assertEquals(strings.without('foobar', 'ob'), 'far')
-        
+        self.assertEqual(strings.without('abba', 'a'), 'bb')
+        self.assertEqual(strings.without('abba', 'cd'), 'abba')
+        self.assertEqual(strings.without('foobar', 'ob'), 'far')
+
     def test_count_discint_chars(self):
         # implement strings.count_distinct that will count the number of distinct characters
-        self.assertEquals(strings.count_distinct('abba'), 2)
-        self.assertEquals(strings.count_distinct('abcdfda'), 5)
+        self.assertEqual(strings.count_distinct('abba'), 2)
+        self.assertEqual(strings.count_distinct('abcdfda'), 5)
 
     def test_tokens(self):
         # implement strings.tokens that will return a list of substrings split from . and removes empty tokens
-        self.assertEquals(strings.tokens('foo.bar'), ['foo', 'bar'])
-        self.assertEquals(strings.tokens('..foo..bar..'), ['foo', 'bar']) 
-        self.assertEquals(strings.tokens('hello'), ['hello'])
+        self.assertEqual(strings.tokens('foo.bar'), ['foo', 'bar'])
+        self.assertEqual(strings.tokens('..foo..bar..'), ['foo', 'bar'])
+        self.assertEqual(strings.tokens('hello'), ['hello'])
 
 if __name__ == '__main__':
     unittest.main()
